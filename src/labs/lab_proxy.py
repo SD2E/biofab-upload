35163f068937198690a47d3b2c4d4bc199122431
Ben Keller <bjkeller@uw.edu>
2018-07-05 09:25:30 -0700

    Change lab proxy to abstract base class

--- src/labs/lab_proxy.py
+++ src/labs/lab_proxy.py
@@ -1,8 +1,25 @@
-class LabProxy:
+import abc
+from typing import List
 
-    def get(self, sample_uri: str):
-        pass
 
-    @staticmethod
-    def create_proxy(lab: str):
-        return {}
+class LabProxy(abc.ABC):
+
+    @abc.abstractmethod
+    def get_bucket_path(self):
+        """
+        Return lab specific bucket path.
+        """
+
+    @abc.abstractmethod
+    def get_fcs(self, sample_uri: str):
+        """
+        Return the file for sample.
+        Returns `None` if there is none.
+        """
+
+    @abc.abstractmethod
+    def get_spectrophotometry(self, sample_list: List[str]):
+        """
+        Return a file which is a table of plate reader measurements for the file.
+        Returns the empty list if there are none.
+        """
