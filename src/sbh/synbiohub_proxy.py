5f1a3cfd6543df2f2ac9e89013251823989b9ede
Ben Keller <bjkeller@uw.edu>
2018-07-06 15:14:14 -0700

    Move sbh_adapter into proxy object

--- src/sbh/synbiohub_proxy.py
+++ src/sbh/synbiohub_proxy.py
@@ -1,3 +1,9 @@
+from synbiohub_adapter.upload_sbol import SynBioHub
+
 class SynBioHubProxy:
+
+    def __init__(self, *, uri, username, password, api):
+        self._sbh = SynBioHub(uri, username, password, api)
+
     def get_operator(self, *, plan_uri, step_id):
         pass
